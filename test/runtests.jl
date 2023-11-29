daba1ea76b92c2cedb5f6b7d609fb8784f3c6499
Benjamin Chu <benchu99@hotmail.com>
2023-11-29 00:33:42 -0800

    add one test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
-using GhostBASIL
+using ghostbasil
 using Test
 
-@testset "GhostBASIL.jl" begin
-    # Write your tests here.
+@testset "block_group_ghostbasil" begin
+    p = 10 # number of features
+    m = 5  # number of knockoffs
+    x = randn(p, p)
+    y = randn(p, p)
+    Ci = x'*x
+    Si = y'*y
+    r = randn(6p)
+    lambda_path = [0.1, 0.05, 0.01, 0.001]
+    beta_i = ghostbasil.block_group_ghostbasil(Ci, Si, r, lambda_path, m=m)
+
+    @test length(beta_i) == (m+1)*p
 end
